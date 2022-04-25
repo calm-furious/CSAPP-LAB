5500248371747bcd19026bcfd56900c24f9ad4e0
Junchi Chen <juncchen@student.ethz.ch>
2022-04-26 00:45:17 +0200

    queue based csim

--- 4 cachelab/csim.c
+++ 4 cachelab/csim.c
@@ -5,6 +5,34 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <string.h>
+#include <assert.h>
+
+#define DECL_LIST(typename) struct typename *prev, *next;
+
+#define INSERT_AFTER(head, pos, elem)                       \
+    (elem)->prev = pos;                                     \
+    if (pos)                                                \
+        (elem)->next = (pos)->next;                         \
+    else {                                                  \
+        (elem)->next = *(head);                             \
+        *(head) = elem;                                     \
+    }                                                       \
+    if((elem)->next)                                        \
+        (elem)->next->prev = elem;                          \
+    if((elem)->prev)                                        \
+        (elem)->prev->next = elem;
+    
+#define DELETE_THIS(head, elem)             \
+    if((elem)->next)                        \
+        (elem)->next->prev = (elem)->prev;  \
+    if((elem)->prev)                        \
+        (elem)->prev->next = (elem)->next;  \
+    if(!(elem)->prev)                       \
+        *(head) = (elem)->next;
+
+#define REPLACE_WITH(head, pos, elem)       \
+    INSERT_AFTER(head, pos, elem);          \
+    DELETE_THIS(head, pos)
 
 int hits = 0, misses = 0, evictions = 0;
 int s=0, E=0, b=0;
@@ -21,9 +49,9 @@ struct cache_line{
     //more
     uint8_t valid;
     int tag;
-    uint64_t counter;
+    DECL_LIST(cache_line);
 };
-struct cache_line* cache; 
+struct cache_line** cache; 
 
 void getCacheOps(int argc, char* const* argv, int* sp, int* Ep, int* bp, FILE** filep){
     // extern char *optarg;
@@ -46,12 +74,22 @@ void getCacheOps(int argc, char* const* argv, int* sp, int* Ep, int* bp, FILE** 
     }
 }
 
-void perform(int op, uint64_t addr){
-    //get tag, set. 
-    uint64_t tag, set;
-    uint64_t set_flag = S-1;
-    set = (addr >> b) & set_flag;
-    tag = (addr >> b) >> s;
+
+void init_cache(){
+    for (int i=0;i<S;i++){
+        cache[i] = (struct cache_line*)NULL;
+        for (int j=0;j<E;j++){
+            struct cache_line *cl = malloc(sizeof(struct cache_line));
+            cl->next = NULL;
+            cl->prev = NULL;
+            cl->tag = 0;
+            cl->valid = 0;
+            struct cache_line **head = &cache[i];
+            INSERT_AFTER(head,(struct cache_line*)NULL,cl);
+        }
+    }
+}
+/*
     DEBUG_PRINTF("set:%lx tag:%lx ",set,tag);
     struct cache_line* free = NULL;
     struct cache_line* victim = NULL;
@@ -105,6 +143,59 @@ void perform(int op, uint64_t addr){
         victim->counter = now;
         victim->tag = tag;
     }
+*/
+
+
+void perform(int op, uint64_t addr){
+    //get tag, set. 
+    uint64_t tag, set;
+    uint64_t set_flag = S-1;
+    set = (addr >> b) & set_flag;
+    tag = (addr >> b) >> s;
+    assert(set<S);
+
+    struct cache_line* itr = cache[set];
+    while(itr){
+        if(!itr->valid){
+            // free = itr;
+            itr->valid = 1;
+            itr->tag = tag;
+            DELETE_THIS(&cache[set],itr);
+            INSERT_AFTER(&cache[set],(struct cache_line*)NULL,itr);
+            DEBUG_PRINTF("miss ");
+            misses++;
+            if(op == (READ|WRITE)){
+                hits++;
+            }
+            return;
+        }
+        if(itr->valid && tag == itr->tag){
+            // hit
+            // bring to front
+            DELETE_THIS(&cache[set],itr);
+            INSERT_AFTER(&cache[set],(struct cache_line*)NULL,itr);
+            DEBUG_PRINTF("hit ");
+            hits++;
+            if(op == (READ|WRITE)){
+                hits++;
+            }
+            return;
+        }
+        if(itr->valid && itr->next==NULL){
+            // miss & evict
+            itr->tag = tag;
+            DELETE_THIS(&cache[set],itr);
+            INSERT_AFTER(&cache[set],(struct cache_line*)NULL,itr);
+
+            DEBUG_PRINTF("miss eviction ");
+            misses++;evictions++;
+            if(op == (READ|WRITE)){
+                hits++;
+            }
+            return;
+        }
+        itr = itr->next;
+    }
 
 }
 
@@ -138,8 +229,8 @@ int main(int argc, char * const argv[]){
     DEBUG_PRINTF("%d, %d, %d\n", s,E,b);
     S = 1<<s;
     B = 1<<b;
-    cache = malloc( S*E*sizeof(struct cache_line) );   //struct cache_line cache[S][E] 
-    memset(cache,0,S*E*sizeof(struct cache_line));
+    cache = malloc( S*sizeof(struct cache_line*) );   //struct cache_line cache[S][E]
+    init_cache();
     simulate(hits, misses, evictions);
     printSummary(hits, misses, evictions);
     return 0;
